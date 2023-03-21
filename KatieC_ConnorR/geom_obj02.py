72715a68a88f4fe0743083ae5df2d8de95c7bbb0
acgis-robe0854 <robe0854@algonquinlive.com>
2023-03-21 16:40:06 -0400

    finished second one

--- KatieC_ConnorR/geom_obj02.py
+++ KatieC_ConnorR/geom_obj02.py
@@ -2,7 +2,7 @@ import sys
 import arcpy
 
 in_txt = r"canada_min.txt"
-out_shp = r"..\..\..\..\data\canada_week11\canada_test.shp"
+out_shp = r"..\..\..\..\data\canada_week11\canada_test_wkt.shp"
 
 def main():
     global arcpy
@@ -20,28 +20,26 @@ def main():
 def txt_to_polyline(in_txt,out_shp):
     arcpy.env.overwriteOutput =  True
     sr = arcpy.SpatialReference(4326)
-    # feature_dict = {}
     features = []
-    # this is a list of polylines
     with open(in_txt,'r') as in_file:
         next(in_file)
-        # this_is_the_end = "END"
-        # in_file.writerow(this_is_the_end)
-        # reader = in_file.read()
-        polyline = arcpy.Array()
-        # this ia list of points for a given polyline
-        
+
+        # polyline = arcpy.Array()
+        coords = ''
+
         for row in in_file:
             if '-' in row:
-                coords= arcpy.Point(float(row.split(' ')[0]), float(row.split(' ')[1].strip('\n')))
-                polyline.append(coords)
-            # elif row == "END":
-            #     features.append(arcpy.Polyline(polyline))
-            #     polyline = arcpy.Array()
+                coords+=row.strip('\n')
+                coords+=','
+                # coords= arcpy.Point(float(row.split(' ')[0]), float(row.split(' ')[1].strip('\n')))
+                # polyline.append(coords)
+
             else: 
-                features.append(arcpy.Polyline(polyline))
-                polyline = arcpy.Array()
-    
+                coords = coords.strip(',')
+                features.append(arcpy.FromWKT(f'LINESTRING ({coords})'))
+                coords = ''
+        coords = coords.strip(',')
+        features.append(arcpy.FromWKT(f'LINESTRING ({coords})'))
     new_fc = arcpy.CopyFeatures_management(features,out_shp)
     arcpy.DefineProjection_management(new_fc,sr)
 

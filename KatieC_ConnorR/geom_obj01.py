948a217c61243f27daf17ea93d9815c050b54c91
acgis-robe0854 <robe0854@algonquinlive.com>
2023-03-28 15:35:41 -0400

    added configurations and time test

--- KatieC_ConnorR/geom_obj01.py
+++ KatieC_ConnorR/geom_obj01.py
@@ -1,13 +1,15 @@
 import sys
 import arcpy
+import time
+start = time.perf_counter()
 
-in_txt = r"canada_min.txt"
-out_shp = r"..\..\..\..\data\canada_week11\canada_test.shp"
+# in_txt = r"canada_min.txt"
+# out_shp = r"..\..\..\..\data\canada_week11\canada_test.shp"
 
 def main():
     global arcpy
 
-    if sys.argv != 3:
+    if len(sys.argv) != 3:
         print("Usage: geom_obj01.py in_txt out_shp")
         sys.exit()
 
@@ -39,6 +41,7 @@ def txt_to_polyline(in_txt,out_shp):
         
     new_fc = arcpy.CopyFeatures_management(features,out_shp)
     arcpy.DefineProjection_management(new_fc,sr)
+    print(f'Elapsed: {time.perf_counter() - start:.1f} s')
 
 
 # txt_to_polyline(in_txt,out_shp)
--- KatieC_ConnorR/geom_obj02.py
+++ KatieC_ConnorR/geom_obj02.py
@@ -1,13 +1,15 @@
 import sys
 import arcpy
+import time
+start = time.perf_counter()
 
-in_txt = r"canada_min.txt"
-out_shp = r"..\..\..\..\data\canada_week11\canada_test_wkt.shp"
+# in_txt = r"canada_min.txt"
+# out_shp = r"..\..\..\..\data\canada_week11\canada_test_wkt.shp"
 
 def main():
     global arcpy
 
-    if sys.argv != 3:
+    if len(sys.argv) != 3:
         print("Usage: geom_obj01.py in_txt out_shp")
         sys.exit()
 
@@ -39,9 +41,10 @@ def txt_to_polyline(in_txt,out_shp):
         features.append(arcpy.FromWKT(f'LINESTRING ({coords})'))
     new_fc = arcpy.CopyFeatures_management(features,out_shp)
     arcpy.DefineProjection_management(new_fc,sr)
+    print(f'Elapsed: {time.perf_counter() - start:.1f} s')
 
 
-txt_to_polyline(in_txt,out_shp)
+# txt_to_polyline(in_txt,out_shp)
 
 if __name__ == "__main__":
     main()
